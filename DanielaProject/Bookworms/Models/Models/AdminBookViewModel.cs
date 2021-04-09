766983f5743f22b796036172d781ec3390ab092e
DanielaElena3 <56530158+DanielaElena3@users.noreply.github.com>
2021-04-09 11:21:02 +0300

    Add files via upload

---
+++ DanielaProject/Bookworms/Models/Models/AdminBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookWorms.BusinessLogic.DataModel;
+using Microsoft.PowerBI.Api;
+
+namespace BookWorms.Models.Admins
+{
+    public class AdminBookViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
