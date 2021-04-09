5f827249bf29c3380777709dbf3f2cdbf651969d
DanielaElena3 <56530158+DanielaElena3@users.noreply.github.com>
2021-04-09 11:23:49 +0300

    Add files via upload

---
+++ DanielaProject/Bookworms/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using BookWorms.DataAccess;
+using BookWorms.Models.Admins;
+using BookWorms.BusinessLogic.DataModel;
+using BookWorms.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace BookWorms.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly AdminService adminsService;
+        public AdminController(UserManager<IdentityUser> userManager, AdminService adminsService)
+        {
+            this.userManager = userManager;
+            this.adminsService = adminsService;
+        }
+
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddBook([FromForm] AdminAddBookViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var userId = userManager.GetUserId(User);
+                adminsService.AddBook(userId, model.Name, model.Description);
+
+                return Redirect(Url.Action("Index", "Admin"));
+            }
+            catch(Exception)
+            {
+                return BadRequest("Invalid request received");
+            }
+
+        }
+
+
+        
+    }
+}
